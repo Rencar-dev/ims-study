8647dd84149b9c74583a2acb37807f561376c279
Seokhwanwon <sh.won@rencar.co.kr>
2023-04-27 18:51:23 +0900

    add: 지워지는지 확인

--- HeadFirstDesignPatterns/chapter_02/parker/index.ts
+++ HeadFirstDesignPatterns/chapter_02/parker/index.ts
@@ -1,3 +1,4 @@
 import { WeatherStation } from "./sementicClass/WeatherStation";
 
 const weatherStation = new WeatherStation();
+
--- HeadFirstDesignPatterns/chapter_02/parker/sementicClass/WeatherStation.ts
+++ HeadFirstDesignPatterns/chapter_02/parker/sementicClass/WeatherStation.ts
@@ -1,12 +1,23 @@
 import { WeatherData } from "./WeatherData";
 import { CurrentConditionDisplay } from './CurrentConditionDisplay';
+import { HeheConditionDisplay } from "./HeheConditionDisplay";
 export class WeatherStation {
   constructor(
     public weatherData = new WeatherData()
   ) {
     const currentDisplay = new CurrentConditionDisplay(this.weatherData);
+
+    const currentDisplay2 = new HeheConditionDisplay(this.weatherData);
+
+    
+
+    this.weatherData.setMeasurements(Math.round(Math.random()), Math.round(Math.random()), Math.round(Math.random()));
+      setInterval(()=>{
+        this.weatherData.setMeasurements(Math.round(Math.random()), Math.round(Math.random()), Math.round(Math.random()));
+        weatherData.removeObserver(currentDisplay);
+      },500)
     // etc display... 
 
-    this.weatherData.setMeasurements(80, 65, 30.4);
+    
   }
 }
